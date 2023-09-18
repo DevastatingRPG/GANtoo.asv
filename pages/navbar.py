84dc91bc5be28ab6f3e92ebfb5682722d4f6a179
DevastatingRPG <devastatingrpg@gmail.com>
2023-09-18 09:52:24 +0530

    removed more files

--- pages/navbar.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import home, navbar, about
-def render():
-    nav_options = ["Home", ":sparkles: Services", "About", "Login"]
-
-    option = st.sidebar.radio("Navigate: ", nav_options)
-
-    return option
